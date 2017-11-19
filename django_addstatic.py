a73f83dcbb1cde823ac84864ab82f238b96a5468
cilame <2473495041@qq.com>
2017-11-19 19:15:23 +0800

    django_addstatic add

---
+++ django_addstatic.py
@@ -0,0 +1,20 @@
+import os,re
+
+if not os.path.isdir('new'):
+    os.makedirs('new')
+
+def gethtmldirs():
+    return filter(lambda i:i.endswith('.html')or
+                         i.endswith('.htm'),
+                  os.listdir('.'))
+
+def addstatic(htmlname):
+    with open(htmlname) as f:
+        s = f.read()
+        s = re.sub(r'((src|href)=)("[^"]+?\.(css|js|png|jpg)")','\g<1> "{% static \g<3> %}"',s)
+        with open('new/'+htmlname,'w') as t:
+            t.write(s)
+
+if __name__ == '__main__':
+    for htmlname in gethtmldirs():
+        addstatic(htmlname)
