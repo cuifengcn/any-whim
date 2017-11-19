e5736183428e449c91614051f6ec3c55d578b5d9
cilame <2473495041@qq.com>
2017-11-19 22:05:08 +0800

    django_addstatic add

--- django_addstatic.py
+++ django_addstatic.py
@@ -13,7 +13,8 @@ def addstatic(htmlname):
         s = f.read()
         if len(re.findall('{% load static %}',s))==0:
             s = '{% load static %}\n'+s
-        s = re.sub(r'((src|href)=)("[^"]+?\.(css|js|png|jpg|gif)(?:\?[^"/]+?|)?")','\g<1> "{% static \g<3> %}"',s)
+        s = re.sub(r'((src|href)=)(")([^"]+?\.)(css|js|png|jpg|gif)(")','''\g<1>\g<3>{% static "\g<4>\g<5>" %}\g<6>''',s)
+        s = re.sub(r'((src|href)=)(")([^"]+?\.)(css|js|png|jpg|gif)(\?[^"/]+?)(")','''\g<1>\g<3>{% static "\g<4>\g<5>" %}{{data|default:'\g<6>'}}\g<7>''',s)
         with open('new/'+htmlname,'w') as t:
             t.write(s)
 
