3c3eeb967caded18ba0522c6cd3acdcbafb284e9
cilame <2473495041@qq.com>
2017-12-04 15:39:38 +0800

    add

--- test_orb_opencv3.py
+++ test_orb_opencv3.py
@@ -6,9 +6,9 @@ img1 = cv2.imread('1.jpg')
 img2 = cv2.imread('2.jpg')  
   
 
-orb = cv2.ORB_create(500)    
-kp1, des1 = orb.detectAndCompute(img2,None)  
-kp2, des2 = orb.detectAndCompute(img1,None)  
+orb = cv2.ORB_create(100)    
+kp1, des1 = orb.detectAndCompute(img1,None)  
+kp2, des2 = orb.detectAndCompute(img2,None)  
 
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 matches = bf.match(des1, des2)
@@ -23,9 +23,9 @@ if len(matches)>10:
     plt.imshow(img3)
     plt.show()
 
-    H,_ = cv2.findHomography(src_pts,dst_pts,cv2.RANSAC,5.0)
+    H,_ = cv2.findHomography(dst_pts,src_pts,cv2.RANSAC,5.0)
     wrap = cv2.warpPerspective(img2, H, (img2.shape[1]+img2.shape[1] , img2.shape[0]+img2.shape[0]))
-    rows, cols = np.where(wrap[:,:,0] !=0)
+    rows, cols = np.where(wrap[:,:,0] > 10)
     min_row, max_row = min(rows), max(rows) +1
     min_col, max_col = min(cols), max(cols) +1
     result = wrap[min_row:max_row,min_col:max_col,:]
