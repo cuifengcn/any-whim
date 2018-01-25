33e3838f1d0e1c9acace032f46668cd74b32c205
cilame <2473495041@qq.com>
2018-01-25 16:43:09 +0800

    add

--- auto_wzq/wzq.py
+++ auto_wzq/wzq.py
@@ -3,40 +3,51 @@ from itertools import groupby
 
 #五子棋类
 #包装了下棋函数，判断是否胜利函数以及估值函数
-#下棋函数如果返回值不为False则，返回值为胜者
-#player in [1,2]
+#下棋函数如果返回值不为 False 则返回胜者（player in [1,2]）
 class WZQ:
     def __init__(self, h, w):
         self.s_map = np.zeros((h,w)).astype(np.int32)
         self._lu = np.zeros((h,w)).astype(np.bool8)
         self._ru = np.zeros((h,w)).astype(np.bool8)
+        self._area_eval = np.zeros((h,w)).astype(np.int32)
+        self._scal_eval = self._create_scal(5)
+        self._scal_ecal_large = self._create_scal(9)
+        self.win = False
         self._values = {
-            (1,0,1,1,1,0,1):3000,#活四
-            (0,1,1,1,1,0):3000,#活四
-            (1,1,1,1,1):10000,#活五
-            (1,1,1,1,0):300,#冲四
-            (1,1,1,0,1):300,#冲四
-            (1,1,0,1,1):300,#冲四
-            (1,0,1,1,1):300,#冲四
-            (0,1,1,1,1):300,#冲四
-            (0,1,1,1,0):300,#活三
-            (0,1,1,0,1):200,#眠三
-            (1,0,1,0,1):200,#眠三
-            (1,0,1,1,0):200,#眠三
-            (1,1,1,0,0):60,#冲三
-            (0,0,1,1,1):60,#冲三
-            (1,1,0,1,0):60,#冲三
-            (0,1,0,1,1):60,#冲三
-            (0,1,1,0,0):20,#活二
-            (0,0,1,1,0):20,#活二
-            (0,1,0,1,0):20,#活二
-            (1,0,1,0,0):20,#眠二
-            (0,0,1,0,1):20,#眠二
-            (1,0,1,0,0):6,#冲二
-            (0,0,1,0,1):6,#冲二
-            (1,1,0,0,0):6,#冲二
-            (0,0,0,1,1):6,#冲二
+            (1,0,1,1,1,0,1):400,#活四
+            (0,1,1,1,1,0):400,#活四
+            (1,1,1,1,1):1000,#活五
+            (1,1,1,1,0):80,#冲四
+            (1,1,1,0,1):80,#冲四
+            (1,1,0,1,1):80,#冲四
+            (1,0,1,1,1):80,#冲四
+            (0,1,1,1,1):80,#冲四
+            (0,1,1,1,0):80,#活三
+            (0,1,1,0,1):60,#眠三
+            (1,0,1,0,1):60,#眠三
+            (1,0,1,1,0):60,#眠三
+            (1,1,1,0,0):18,#冲三
+            (0,0,1,1,1):18,#冲三
+            (1,1,0,1,0):18,#冲三
+            (0,1,0,1,1):18,#冲三
+            (0,1,1,0,0):6,#活二
+            (0,0,1,1,0):6,#活二
+            (0,1,0,1,0):6,#活二
+            (1,0,1,0,0):6,#眠二
+            (0,0,1,0,1):6,#眠二
+            (1,0,1,0,0):1,#冲二
+            (0,0,1,0,1):1,#冲二
+            (1,1,0,0,0):1,#冲二
+            (0,0,0,1,1):1,#冲二
             }
+
+    #米字形的矩阵生成，用于优化结构
+    def _create_scal(self, n):
+        c = int(n/2)
+        v = np.zeros((n,n),dtype=np.int32)
+        v[c,:],v[:,c] = 1,1
+        v = np.eye(n,dtype=np.int32)|np.eye(n,dtype=np.int32)[:,::-1]|v
+        return v
     
     #找到一个点所在位置的横竖斜上的所有点以及该点在其array里的坐标
     def _find_crossNslash(self, point):
@@ -61,10 +72,13 @@ class WZQ:
     #确保游戏没有结束
     #确保当前point没有其他落子
     def play_1_round(self, point, player):
+        assert player in [1,2]
         if self.s_map[point] != 0:
             return False
         self.s_map[point] = player
-        return self._jug_win(point)
+        self._area_eval_add(point)
+        self.win = self._jug_win(point)
+        return True
 
     #根据自身修正 array 排除对手棋子的干扰
     def _revise_array(self, idx, arr, player):
@@ -75,7 +89,6 @@ class WZQ:
         v = np.where(arr[idx:]==k)[0]
         maxx = v.min() + idx if len(v) else len(arr)
         return ridx,arr[minx:maxx]
-        
 
     #估值算法
     def evaluate(self, point, player):
@@ -98,24 +111,55 @@ class WZQ:
         self.s_map[point] = 0
         return core
 
+    #需要计算估值的范围
+    def _area_eval_add(self, point):
+        ph,pw = point
+        (u,l) = np.maximum(np.array(point)-2, 0)
+        (d,r) = np.minimum(np.array(point)+3, self.s_map.shape)
+        gu,gl,gd,gr = 2-(ph-u), 2-(pw-l), 2+(d-ph), 2+(r-pw)
+        self._area_eval[u:d,l:r] |= self._scal_eval[gu:gd,gl:gr]
+
+    #test 一层的逻辑
+    def robot_1(self, player):
+        self._temp1_eval_map = np.zeros(self.s_map.shape).astype(np.int32)
+        self._temp2_eval_map = np.zeros(self.s_map.shape).astype(np.int32)
+        for i,j in np.vstack(np.where(self._area_eval==1)).transpose():
+            if self.s_map[i,j] == 0:
+                self._temp1_eval_map[i,j] = self.evaluate((i,j),player)
+        for i,j in np.vstack(np.where(self._area_eval==1)).transpose():
+            if self.s_map[i,j] == 0:
+                self._temp2_eval_map[i,j] = self.evaluate((i,j),3-player)
+        v = (self._temp1_eval_map+self._temp2_eval_map)
+        v = np.vstack(np.where(v==v.max())).transpose()
+        v = v[np.random.choice(range(len(v)))]
+        return v
+        
+
 #test
 if __name__== '__main__':
     wzq = WZQ(15,15)
     player = 2
+    print(wzq.play_1_round((0,6),1))
+    print(wzq.play_1_round((1,4),1))
+    print(wzq.play_1_round((3,2),1))
+    print(wzq.play_1_round((3,7),1))
+    print(wzq.play_1_round((6,4),1))
     print(wzq.play_1_round((1,5),player))
     print(wzq.play_1_round((2,4),player))
     print(wzq.play_1_round((3,3),player))
+    print(wzq.play_1_round((3,5),player))
     print(wzq.play_1_round((4,2),player))
+    print(wzq.play_1_round((4,4),player))
     print(wzq.play_1_round((3,4),player))
+    print(wzq.play_1_round((12,12),player))
     print(wzq.s_map)
     import time
     _t = time.time()
     print(wzq.evaluate((2,2),2))
     h,w = wzq.s_map.shape
     v = np.zeros(wzq.s_map.shape).astype(np.int32)
-    for i in range(h):
-        for j in range(w):
-            if wzq.s_map[i,j] == 0:
-                v[i,j] = wzq.evaluate((i,j),2)
+    for i,j in np.vstack(np.where(wzq._area_eval==1)).transpose():
+        if wzq.s_map[i,j] == 0:
+            v[i,j] = wzq.evaluate((i,j),2)
     print(v)
     print(time.time()-_t)
---
+++ auto_wzq/wzq_gui.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+from tkinter import *
+from functools import partial
+from wzq import WZQ
+
+class GUI_WZQ:
+    def __init__(self,h,w):
+        self.master = Tk()
+        self.master.title('mini_wzq')
+        self.master.resizable(width=False, height=False)
+        self.wzq = WZQ(h,w)
+        self.h = h
+        self.w = w
+
+        self.player = 1
+        self.player_text = {1:'●',2:'〇'}
+        self.player_head = {1:'黑',2:'白'}
+
+        self.headarea = self.create_headarea()
+        self.gamearea = self.create_gamearea(h,w)
+        self.headarea.pack()
+        self.gamearea.pack()
+        self.master.mainloop()
+
+    def reset(self):
+        pass
+
+    def flash(self,i,j):
+        if self.wzq.win:
+            return
+        if self.wzq.play_1_round((i,j),self.player):
+            exec("self.e%d_%d['text']='%s'"%(i,j,self.player_text[self.player]))
+            if self.player==1:
+                self.player = 2
+            elif self.player==2:
+                self.player = 1
+            if self.wzq.win:
+                self.headlabel['text'] = 'win:'+self.player_head[self.wzq.win]
+            else:
+                self.headlabel['text'] = 'next:'+self.player_head[self.player]
+                if self.player==2:
+                    point = self.wzq.robot_1(self.player).tolist()
+                    self.flash(*point)
+
+    def create_headarea(self):
+        head = Frame(self.master)
+        self.headlabel = headarea = Label(head,text='next:'+self.player_head[self.player])
+        resetbot = Button(head,text='reset',font=('黑体',9),relief='groove',command=self.reset)
+        headarea.pack(side=LEFT)
+        resetbot.pack(side=RIGHT)
+        return head
+
+    def create_gamearea(self,h,w):
+        gamearea = Frame(self.master)
+        for i in range(h):
+            for j in range(w):
+                exec("self.e%d_%d = Button(gamearea,text='  ',font=('黑体',7),width=1,relief='groove',command=partial(self.flash,%d,%d))"%(i,j,i,j))
+                exec("self.e%d_%d.grid(row=i,column=j,sticky=W+E+N+S)"%(i,j))
+        return gamearea
+
+
+if __name__ == "__main__":
+    v= GUI_WZQ(19,19)
+
+
+        
