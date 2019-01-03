21e23bf219623717fd14ef963e0ce687a1e6a580
cilame <2473495041@qq.com>
2019-01-03 14:58:46 +0800

    Update 获取网站文本.py

--- 获取网站文本.py
+++ 获取网站文本.py
@@ -1,30 +1,36 @@
-import re
-
+# -*- coding:utf-8 -*- 
 import requests
+import re
 from lxml import etree
 
-url = 'http://www.gov.cn/yjgl/2006-01/15/content_159236.htm'
-s = requests.get(url)
-
-def normal_content(content,tags=['script','style','select','noscript',]):
-    try:
-        content = content.decode('utf-8')
-    except:
-        content = content.decode('gbk')
-    e = etree.HTML(content)
+def normal_content(content,
+                   tags=['script','style','select','noscript'],
+                   rootxpath='//html'):
+    # 通用文本提取函数
+    if type(content) is bytes:
+        try:
+            c = content.decode('utf-8')
+        except:
+            c = content.decode('gbk')
+    elif type(content) is str:
+        c = content
+    else:
+        raise 'content type must in [bytes, str].'
+    # 针对部分网页汉字粘连的问题的处理，增强鲁棒性。
+    c = re.sub('([^>]*[\u4e00-\u9fa5]{1,}[^<]*)','\g<1> ',c)
+    e = etree.HTML(c)
     q = []
-    for i in e.getiterator():
-        if i.tag in tags:
-            q.append(i)
-    for i in q:
-        i.getparent().remove(i)
-    s = e.xpath('string(//html)')
-    s = re.sub('\t+','',s)
-    s = re.sub('\r+','',s)
-    s = re.sub(' +',' ',s)
-    s = re.sub('\n+','\n',s)
-    s = re.sub('(:?\n )+','\n ',s)
-    return s
-        
-v = normal_content(s.content)
-print(v)
+    for it in e.getiterator():
+        if it.tag in tags or type(it.tag) is not str:
+            q.append(it)
+    for it in q:
+        p = it.getparent()
+        if p is not None:
+            p.remove(it)
+    t = e.xpath('normalize-space({})'.format(rootxpath))
+    return t.strip()
+
+url = 'http://www.pingan.com/official/insurance?secondclass=4617947d7ded73bd&flag=47b8b71a21e9494e'
+s = requests.get(url)
+t = normal_content(s.content)
+print(t)
