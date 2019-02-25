a9e000a6e1982c107912e76fc07050738cb6e749
cilame <2473495041@qq.com>
2019-02-25 23:40:04 +0800

    Update pdfparse.py
    了解了部分io流的使用方式，不用再生成临时文件来处理问题数据。

--- pdfparse.py
+++ pdfparse.py
@@ -3,31 +3,18 @@
 from pdfminer.pdfinterp import PDFResourceManager,process_pdf
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
-from io import StringIO
-import tempfile, os
+from io import StringIO, BytesIO
 
 import requests
 r = requests.get("http://www.commonlii.org/sg/journals/SGYrBkIntLaw/2006/19.pdf")
 
-def readPDF(bitfile,filename='temp'):
-    def mk_file(filename,bitfile):
-        path = tempfile.mkdtemp()
-        pfile = path + filename
-        with open(pfile,'wb') as f:
-            f.write(bitfile)
-        return open(pfile,'rb'),pfile
-    f,pfile     = mk_file(filename,bitfile)
+def readPDF(bitfile):
     rsrcmgr     = PDFResourceManager()
     retstr      = StringIO()
     laparams    = LAParams()
     device      = TextConverter(rsrcmgr,retstr,laparams=laparams)
-    process_pdf(rsrcmgr,device,f)
+    process_pdf(rsrcmgr,device,BytesIO(bitfile))
     content     = retstr.getvalue()
-    device.close()
-    retstr.close()
-    f.close()
-    if os.path.exists(pfile):
-        os.remove(pfile)
     return content
 
 bitfile = r.content
