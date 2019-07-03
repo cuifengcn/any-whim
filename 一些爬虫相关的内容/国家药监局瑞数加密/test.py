017c64d8894a8a1d3e55434722a315f5a3ae00a9
cilame <opaquism@hotmail.com>
2019-07-04 00:48:47 +0800

    增加瑞数解密的尝试，目前尚未解密。

---
+++ 一些爬虫相关的内容/国家药监局瑞数加密/test.py
@@ -0,0 +1,83 @@
+s = '''LwiPhsqht=hu9hj9huLhk[huIhu_ho^hkyhnu&Qzkqttwefuuq~eDeeievwhpqrItJ}ym\\OnMFu6x[^PZ8WKRV|=NLkUT>7g{QSo3<(]GfXsz5YH;l~d9_j:i)+*, !#$%-./012?BDE`bce}ehjnqehexqnhjehtshfyef)N)m)e&>ys6m|uXsHk{h:{qrji(uQt6ikqhgVrkuKk6lZr]nsjvjQr~nKjLoLkOpqh6g]pVpxgiivk\\yxj<tho_p:r8n7g|p|u=o7xzpRq(sSl)phups{%-};.4M]Yd`C}dZ8^MVOZ3FAZ`M``9LV}RVV<9LM>zeT|zgyM70]SBF;vMat;|>8CMaBeb>8Sg?4MCcD;a8REM`Yf_`Y4R5tYMa0g[3vR=:NMDF@/BZ;a8Ov4?0{M:[]/UuNM5FR35vRdFZvuMVX?9VO9z6\\>0M?`D[DNM?cNW|<]374MF<d2DbTM?=T@6\\9E:Xw@7\\9;64MUP4M71d;FOd9?4Mw`9xaP9[MCX>}M`}NQ]tv.5z;V5t>M5ZeS8}R4FFg3C8e781g{?td3`8_FwvZ58A_]u8}MwbCMCYg7V}gDu}Z15NMFB}A5`40]ASb.`tT7}>T7zt0]`@X]A4Tu`.QuX]\\V^M70]8?`;6?F?<?AedMVFRaM]C?Y`tGMU\\7DU0_t?\\F1w0@|5beNCOv:wB765t{M>t@N>8TW>z7@64M|bCMFv2M8b^[7<}M?F{A7B{GM8XF1D`@ZDC.AM7ZF8?NM:}YM6cZ0d1}MBb{E5`Tx7[^zU}^4?<TzUXt[VXd2M`}tQ`=fX]bRe`tTM`bwMUO?;w.MVvf4B8edwOd|CNMUOZ6:}f<?AGMUZg:2AR9B8R/C\\F{w.M3[RAaz4{a4MF`dP7}S^VYZ\\%*-RF,-6x7y6Ix_vrL{*`A=Lw,Psnf_H-CP@NE_bN}<.v,Fq~AFo>_M[xC/}e]/</EM)^970,/w^JLt_,9Hf~]yozE`PII~<?I-a`K*eJ^PPI]M8?A(_xI{~nqMMIEN?nc7Dr*`:MACy6u|*Er8wD?c,LscH(+B]<N^0d^KOx,*PyrEB{n_Hcv+dD+|;8NB-)?L-|>xC=ME(??,0]FEOHzpM}^+){/,(,^C<Jw,78(/{.zqHO>L{^-p*Gs_*C+*B0w|tc-9PfJ9{+%EOGI8ITTLIRIxIlImIbTx/UIMI_T3crI1TsTIIUxJTMxpIdRWTPTJT#TtT@ThT(cWTXT$TyiWTwT1TCT*mjT><bR%TYTKTTcsT+m>R TZR>x\\TgT[U_R#x&x0xkT/T8x3bk_ix isRyx.CjxXTUkrTlclTiRbxZC8xPm}Fw_FCWshUrRkx-xtcps(kLx[c}s>xix}/-cUctxVF|cKF,chxxm5Fhd%xMR}FdRtk 1}ikFtilx8dgxssP@lb~xmb-slxFRgUNx@RPsYC*xCi8R/kB/5R8i#x/cFC[R5d2>%<XR|b(<+i0R3RMRKRhs#F /[iBc&cwmt/#/hdPR0cLsKc|R@mCCP1i>>d$cRc*m1bys2cYccF<U\\@\\Rs_w/0c(sIcCc~Fl1FsCcN/McMcic-C#@.FpmKcBd|_UmNsic#cgmclxs./%MNUVb@mTm*>PFyCCFil2bp>ldyU*swcbl>Ug>j>pmmmJlh<51.10U11LF+F}m<d/sFm$bdc1c@mxkNiPlB/8U#mB1Bmd1~b|m%m0kmdJbimwmU<\\U-iRmrb\\lr<hmsMW@UFcF2F#MY1|mPMxmg1d/g1U<JFFC3@%1#1IUy1b_t<M/|/(@JCsUd/mUpU/bcF\\kKly@&>0F*<YF%Utb+FBM~>2i\\M|b3M[1kkVd3@<_@MP_N_&/iktUldX>BUTF_bIi[U,<ddBd,Mw>J/bM\\s,bCiL@sb<MF/@@rUi1g1xF8l0<0lml(/I/db_b#FM<2FTFCsXi.x1xbF$dbFs_M/VMk@VCgU.@k_(C.>#_gUB_d1Z<~1s@}bKl/MB@5<8@YUsjiipkJkb_#b1_%_*1m/}l}dRU&U5/2bhi1>8M}Mdi3>ybM1YC+dCC%$}kUCURJ>K@xls>V@clLl-1(_b<j@,/<kZdM_B<FM8ksd~<3@1@t>ZC}Uj@/@diw<lk._$C$_Kd _[_5UPdms__pUIbV_h1TC&T<k/d#bB@pd1<Ci~k1bg1psjk5M_/F/Bk8Mg/ZlZ1N><Cpi5>~>ciddF1PRc/t@Cl|>&k(M$iI>Ib,l$cx>W<#_Ys\\>Cb>_Tkyk,kdC<s|l.ki<ZkF>.UckCF.l+_,<NlI<PU>dUM(kpk$>11\\1XsW_X1<kRkWlTsdsNcy>LU[RI_Pl*<VkIMV1li<dT>h<xm\\> i|>@Uh<%UKs/d0@2UCC@>_mX<>>|M1s0<U/\\_kM/@(C|/Csg</M<<w@R>ddYs8<p11M2d8kcMyiYlglP>+sBi/CIirMrCklNUXUWd.>FM@Mhigi><Tl,/jMK<kk0<skgMTdZ1rmps3d(M#Mcd\\CyFZMRM.1yb&<yl M-U(icl&CVd5<,C2@>lJCN>UlXkwk}>Mssiy>t<[lFUYMjlwsxUks5xR_+l_<.M0MXU%d}k[FXU3<ts@kxM>MMUxiUmRd&<B>xUUlk_VdiF(s%@~Cw<WsL@-i-@h<rk+kMMZlMscRUlbCdl#li/N< lC>YkXdtiT<RkTl~C-sJ@ >g1+sVU2_Csy_<lKU}18sRl<>wFx1 @yU+sMltk~_c/ps&Uw>sc>1RCRi%TFc[>msZdLx+U<_I1>l8R\\MI>Xl3bR/yk21C>*/*>NCxC\\1csk/Xk>kkM5/3dx@Z>k<_s}iVlp<&Mli@R(@+M @b_\\1VCmC0M&/,k-@miN1jUR<cldiZ1[l[CTm#1-R,@NM3l@1hlV_xi(RZxBlU1/_j/P_R/Tk|>TbT@3sm<KMC@#15mV<iML@8@W>rk&_JMb/$C~FPb5U~dI/~<|@BbL/Rb/iX_ss~_.d@d+bxR&_-b$U @Xl1U$/xF~ChMpd>d<<$Cr_y1t<-MtkhCb<L_L_~k@M%<(F3>ill/Wdr/J__MJ@w1@@jbwb0>$b UFFkb*d_Mm<g/ _8/.dp_rm&_>_/FR@KbJ1&ULF[k%@$>5ds1w<@1$bFimbY1KsbkjbrFLF&c<lcm@djCl<1>(bU@Tdki,sTc5l%FY<mCK>-d*it_3c2st>}k\\@0ij_ZCFkYCi/cF@C>_ @@b8bjFI/>1,M+UJ@L_lF/m+mkm8bl/riJi2FUdd_0btFNCXc$m[U0UbbXbWb%Frs-/Lk*1_CJM*klUMbm1Ws[s<FbMsdKF>/K@FdVRib2F513/&lWdcc_l5@[mh/Ymy<*_2@*k_iK_|U@F-cT1*s*_W12F0_1>b@i@MibmbmFU8mMs1M,m(bbi}kP//c\\UZ@|c C,s k3m.cIm3C_UmcdFgcj/+k#cPFV@_>/C Fmdlc/MU<<dW/sc0FJR1ckCc/kmlm2sp1%>[RlF1cVU|m,sUdNd-k<d[RCRxcJ<Ic81MmYR+cXRTRNc3C5c%C/m~bNRFFjR$R-RVR_lj@PmWc.bZlYiFbs/1FWs+m_Rw>RFKb[CYRLmicmR[_}R2xUR*RXdh/li srm-bPi_s$RrMiRRR~xNC1/_>,x<CBi&>\\1JR<CLm|xhxcx#<}x~Rj@gxjRdRmcZxWi*xYx*xdi+Ctxgb.RYxwdwm/Rpm x%x,CMTdxlxKx$mLl\\_mx5x_c,xITcRBTmi$x2ihx(xLxyc+x|iMx>T2mITBTR@IT%T~TNT}CZTbR.T\\TVT0TrT_xrT&T /wT5mZTkT,b}ixC(TWICT-T.IsxTTjIkI/T|IiI@I<IcIFI>TpIII+?,022\\--04\\-./1\\1/.\\--041oAGEpEBpERo{r~Tc~IFRppW,F+|)XQ}2tHIwgecjsjpzHS|wBVr0{qqqqqr1qVr5VC0koUWzl66khwWwgCqqqqqqqKxsr8Yd1BCLmeMNJgFZXfQqqr0qhx3t0c.hZK1hqk674qt1074790464qh5r5eVwSMwk5qJ1561560447373q.7WyOlUs.EnOr3M1yH2ei3SzgvfVVjTQDWLw0.D7Vwk9q{qO_n_GTnYrw9kbmmBRHhImcU.9slEIbwzmatr2zpC1n7SpQjvP_5w7iVPbbt.o84gqkMigqkqu0q8KSrCECGbFPw.Eb0vMPY.Eb7GF1NOhGq}yFTRAJS7vKv3EHaZ4psalt9V5rKpsMTqrDSmRsqGmkaYmqGAApAAmV2JRVq9mkaNccq7Hk0VErqRmlGYEV0JArqWVDpzmDAGYq2QEmmmkK0QFrmNADr7t.GuXoFaE4YDZK4g3yaMTp87J4Aj9kiAwLGOOVMRMzrOfp8EJeGjekH7wXaOgVFeMLSuXohAmefBepILl_lUZrRNs9Yqqc82qD9a1adcq{IQv2kA0rH1rVscf7FlaLwkYuTq8AqufHzmIlsgfIbA4TqXVOvldaM4rHgoIeQ4ahdxHVqgfBeoHJq.9F91H9QgS6zlIlUer3qm65185qqq{|1Y7eiP7ahrAz36zjMSALqnVoraE9qUASccAmEvzwMS0GoPViwl9hYDaLclLhAba6cmQJAKaTQmqtADaTI2g.DGq8wlNZsvrJIV2gso_C8hJg7UX_I4w4z6_dIheyBv5yIAqHsFmd9U_KIyRZ9o9q.llqdxolTQsmTEbEv8cR5EvmLwP2uhcJ2QPNOtsgv3ugqKOLvFjlfcq7QMpTwG6r10ZAqliI0Kqqqqq'''
+
+
+i = 0
+
+def som(s):
+    global i
+    v = some(s)
+    c = s[i:i+v]
+    i += v
+    return c
+
+def some(s):
+    global i
+    v = ord(s[i])
+    if v >= 40:
+        i += 1
+        return v - 40
+    else:
+        kv = 39 - v
+        v = 0
+        for j in range(kv):
+            v *= 87
+            v += ord(s[j+1+i]) - 40
+        i += kv + 1
+        return v + 87
+        
+def somee(ss):
+    a = len(ss)
+    b = ord(ss[0]) - 40
+    c = ''
+    for i in ss[1:]:
+        v = ord(i)
+        if v >= 40 and v < 127:
+            v += b
+            if v >= 127:
+                v = v - 87
+        c += chr(v)
+    return c
+
+a = som(s)
+b = som(s)
+a = somee(a).split('`')
+b = somee(b).split('`')
+c = list(zip(a,b))
+for _ in c:
+    print(_)
+
+def someee(ss):
+    a = ss.split(c[3][1]) if c[3][1] else list(ss)
+    for i in range(0,len(a)-1,2):
+        a[i],a[i+1] = a[i+1],a[i]
+    return c[3][1].join(a)
+
+def split_by_num(ss, num):
+    ret = []
+    for i in range(len(ss)//num):
+        ret.append(ss[i*num:(i+1)*num])
+    return ret
+
+v1 = som(s)
+v2 = som(s)
+v3 = som(s)
+v4 = som(s)
+v5 = som(s)
+v6 = som(s)
+print()
+print(v1) # 太短的加密被放弃了
+print(v2)
+print(v3)
+print()
+print('加密===',v4)
+print('解密===',split_by_num(somee(v4), 2))
+print()
+print('加密===',v5)
+print('解密===',somee(v5).split('`'))
+print()
+print('加密===',v6)
+print('解密===',somee(v6).split('`'))
+d = split_by_num(somee(v4), 2)
+e = somee(v5).split('`') + somee(v6).split('`')
+for _ in zip(d,e):
+    print(_)
