39078de5eb55e4139d564c054ba8e9adc7f9716f
cilame <opaquism@hotmail.com>
2019-10-18 16:17:36 +0800

    add

--- 关于c语言/tcc_autocreate_she11code.c
+++ 关于c语言/tcc_autocreate_she11code.c
@@ -1,36 +1,39 @@
-// 该脚本生成 32位 的 windows she11code 。
+// 该脚本生成 32位与64位 通用的 windows she11code 。 语法依 at&t 语法，可被 gcc，tcc 编译。
 // 编写 she11code 的代码需要注意的是
 // 1) 不能直接使用函数获取函数的地址，需要通过一定的汇编获取 Kernel32 的地址
 // 2) 后续找到 GetProcAddress LoadLibraryA 这两个函数的地址后续基本就需要这两个函数进行处理必要函数的获取
 // 3) 使用那些函数的时候也需要注意，需要从函数声明的地方获取结构声明，所有“函数声明”不会影响生成的代码的顺序和she11code体积。
 // 4) 另外 she11code 的编写中注意不能使用任何全局变量，并且函数内“字符串声明”的方式只能使用 char[] 来实现，
 //    这是为了避免字符串由于编译器优化被放到其他地方从而 she11code 不完整。
-// cmd> tcc tcc_autocreate_she11code.c -m32
-// 生成的工具 tcc_autocreate_she11code.exe 执行后会生成一个 sh.bin 的 she11code 二进制文件。
+// cmd> tcc tcc_autocreate_she11code.c
+// 需要生成32位则在 tcc 命令行加上 -m 。 生成的工具 tcc_autocreate_she11code.exe 执行后会生成 sh.bin 的文件(she11code)。
 
 #include <stdio.h>
 #include <windows.h>
 
-DWORD getKernel32();
-FARPROC _GetProcAddress(HMODULE hModuleBase);
+HMODULE getKernel32();
+FARPROC getProcAddress(HMODULE hModuleBase);
 void ShellcodeStart();
 void ShellcodeEntry();
 void ShellcodeEnd();
 void CreateShellcode();
 
 // create shellcode bin.
-int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
+// int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
+int main(int argc, char const *argv[]) {
+    printf("kernel32:       %016llx\n", getKernel32());
+    printf("GetProcAddress: %016llx\n", getProcAddress((HMODULE)getKernel32()));
     CreateShellcode();
 }
 void CreateShellcode(){
-    HANDLE hBin = CreateFileA("sh.bin", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
+    HANDLE hBin = CreateFileA("sh64.bin", GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, 0, NULL);
     DWORD dwSize;
     DWORD dwWrite;
     if (hBin == INVALID_HANDLE_VALUE){
         printf("%s error:%d\n", "create sh.bin fail.", GetLastError());
         return;
     }
-    dwSize = (DWORD)ShellcodeEnd - (DWORD)ShellcodeStart;
+    dwSize = ShellcodeEnd - ShellcodeStart;
     WriteFile(hBin, ShellcodeStart, dwSize, &dwWrite, NULL);
 }
 
@@ -78,7 +81,7 @@ void InitFunctions(PFUNCTION pFn){
     char szMessageBoxA[] = {'M','e','s','s','a','g','e','B','o','x','A','\0'};
     char szKernel32[] = {'k','e','r','n','e','l','3','2','\0'};
     char szCreateFileA[] = {'C','r','e','a','t','e','F','i','l','e','A','\0'};
-    pFn->fn_GetProcAddress  = (FN_GetProcAddress)_GetProcAddress((HMODULE)getKernel32());
+    pFn->fn_GetProcAddress  = (FN_GetProcAddress)getProcAddress((HMODULE)getKernel32());
     pFn->fn_LoadLibraryA    = (FN_LoadLibraryA)pFn->fn_GetProcAddress((HMODULE)getKernel32(), szLoadLibraryA);
     pFn->fn_MessageBoxA     = (FN_MessageBoxA)pFn->fn_GetProcAddress(pFn->fn_LoadLibraryA(szUser32), szMessageBoxA);
     pFn->fn_CreateFileA     = (FN_CreateFileA)pFn->fn_GetProcAddress(pFn->fn_LoadLibraryA(szKernel32), szCreateFileA);
@@ -99,7 +102,17 @@ void ShellcodeEntry(){
     WKMessageBox(&fn);
 }
 // get kernel32 and get GetProcAddress.
-__declspec(naked) DWORD getKernel32(){
+#ifdef _WIN64
+HMODULE getKernel32(){
+    asm("mov %gs:(0x60), %rax");
+    asm("mov 0x18(%rax), %rax");
+    asm("mov 0x20(%rax), %rax");
+    asm("mov (%rax), %rax");
+    asm("mov (%rax), %rax");
+    asm("mov 0x20(%rax), %rax");
+}
+#else
+HMODULE getKernel32(){
     asm("mov %fs:(0x30), %eax");
     asm("mov 0x0c(%eax), %eax");
     asm("mov 0x14(%eax), %eax");
@@ -107,50 +120,37 @@ __declspec(naked) DWORD getKernel32(){
     asm("mov (%eax), %eax");
     asm("mov 0x10(%eax), %eax");
 }
-FARPROC _GetProcAddress(HMODULE hModuleBase){
-    PIMAGE_DOS_HEADER lpDosHeader = (PIMAGE_DOS_HEADER)hModuleBase;
-    PIMAGE_NT_HEADERS32 lpNtHeader = (PIMAGE_NT_HEADERS32)((DWORD)lpDosHeader + lpDosHeader->e_lfanew);
-    PIMAGE_EXPORT_DIRECTORY lpExports;
-    PDWORD  lpdwFunName;
-    PWORD   lpwOrd;
-    PDWORD  lpdwFunAddr;
-    DWORD   dwLoop;
-    FARPROC pRet;
-    char* pFunName;
-    if (!lpNtHeader -> OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].Size){
-        return NULL;
-    }
-
-    if (!lpNtHeader -> OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress){
-        return NULL;
-    }
-    lpExports   = (PIMAGE_EXPORT_DIRECTORY)((DWORD)hModuleBase + (DWORD)lpNtHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
-    lpdwFunName = (PDWORD)((DWORD)hModuleBase + (DWORD)lpExports->AddressOfNames);
-    lpwOrd      = (PWORD)((DWORD)hModuleBase  + (DWORD)lpExports->AddressOfNameOrdinals);
-    lpdwFunAddr = (PDWORD)((DWORD)hModuleBase + (DWORD)lpExports->AddressOfFunctions);
-    dwLoop      = 0;
-    pRet        = NULL;
-    for (;dwLoop <= lpExports->NumberOfNames - 1; dwLoop++){
-        pFunName = (char*)(lpdwFunName[dwLoop] + (DWORD)hModuleBase);
-        if (    pFunName[0] == 'G' &&
-                pFunName[1] == 'e' &&
-                pFunName[2] == 't' &&
-                pFunName[3] == 'P' &&
-                pFunName[4] == 'r' &&
-                pFunName[5] == 'o' &&
-                pFunName[6] == 'c' &&
-                pFunName[7] == 'A' &&
-                pFunName[8] == 'd' &&
-                pFunName[9] == 'd' &&
-                pFunName[10] == 'r' &&
-                pFunName[11] == 'e' &&
-                pFunName[12] == 's' &&
-                pFunName[13] == 's'     ){
-            pRet = (FARPROC)(lpdwFunAddr[lpwOrd[dwLoop]] + (DWORD)hModuleBase);
-            break;
+#endif
+FARPROC getProcAddress(HMODULE hModuleBase){
+    LPBYTE lpBaseAddr = (LPBYTE)hModuleBase;
+    PIMAGE_DOS_HEADER lpDosHdr = (PIMAGE_DOS_HEADER)lpBaseAddr;
+    PIMAGE_NT_HEADERS pNtHdrs = (PIMAGE_NT_HEADERS)(lpBaseAddr + lpDosHdr->e_lfanew);
+    PIMAGE_EXPORT_DIRECTORY pExportDir = (PIMAGE_EXPORT_DIRECTORY)(lpBaseAddr + pNtHdrs->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+    LPDWORD pNameArray = (LPDWORD)(lpBaseAddr + pExportDir->AddressOfNames);
+    LPDWORD pAddrArray = (LPDWORD)(lpBaseAddr + pExportDir->AddressOfFunctions);
+    LPWORD pOrdArray = (LPWORD)(lpBaseAddr + pExportDir->AddressOfNameOrdinals);
+    FARPROC GetProcAddressAPI;
+    for (UINT i = 0; i < pExportDir->NumberOfNames; i++){
+        LPSTR pFuncName = (LPSTR)(lpBaseAddr + pNameArray[i]);
+        if (    pFuncName[0] == 'G' &&
+                pFuncName[1] == 'e' &&
+                pFuncName[2] == 't' &&
+                pFuncName[3] == 'P' &&
+                pFuncName[4] == 'r' &&
+                pFuncName[5] == 'o' &&
+                pFuncName[6] == 'c' &&
+                pFuncName[7] == 'A' &&
+                pFuncName[8] == 'd' &&
+                pFuncName[9] == 'd' &&
+                pFuncName[10] == 'r' &&
+                pFuncName[11] == 'e' &&
+                pFuncName[12] == 's' &&
+                pFuncName[13] == 's'     ){
+            GetProcAddressAPI = (FARPROC)(lpBaseAddr + pAddrArray[pOrdArray[i]]);
+            return GetProcAddressAPI;
         }
     }
-    return pRet;
+    return NULL;
 }
 void ShellcodeEnd(){
 }
