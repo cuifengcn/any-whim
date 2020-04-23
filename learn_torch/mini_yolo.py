7f2007e10bb2b373f8e99ad2d7cddcc260e969fa
cilame <opaquism@hotmail.com>
2020-04-23 23:08:19 +0800

    add

--- learn_torch/mini_yolo.py
+++ learn_torch/mini_yolo.py
@@ -36,6 +36,7 @@ def readxml(file, islist=False):
         raise 'fail load img: {}'.format(f)
     size = v.getElementsByTagName('size')[0]
     npimg = cv2.cvtColor(cv2.imread(f), cv2.COLOR_BGR2RGB)
+    npimg.resize((416, 416, 3))
     npimg_ = np.transpose(npimg, (2,0,1))
     def readobj(obj):
         d = {}
@@ -50,6 +51,8 @@ def readxml(file, islist=False):
         d['xmax']   = int(bbox.getElementsByTagName('xmax')[0].firstChild.data)
         d['ymax']   = int(bbox.getElementsByTagName('ymax')[0].firstChild.data)
         d['rect']   = d['ymin'],d['xmin'],d['ymax'],d['xmax']
+        d['ratew']  = d['width']/416
+        d['rateh']  = d['height']/416
         d['centerx'] = (d['xmin'] + d['xmax'])/2.
         d['centery'] = (d['ymin'] + d['ymax'])/2.
         d['numpy']  = npimg_
@@ -79,17 +82,18 @@ def make_y_true(imginfo, S, anchors, class_types):
     ceillen = (5+len(class_types))
     log = math.log
     z = torch.zeros((S, S, len(anchors)*ceillen))
-    for i, (ax, ay) in enumerate(anchors): # 目前这里没有使用 anchors，因为我不会用，陡曾复杂度
+    for i, (aw, ah) in enumerate(anchors): # 这里的 anchor 没有参与处理，因为我暂时不会用，照着公式写仍旧有问题
         left = i*ceillen
         clz = [0.]*len(class_types)
         clz[class_types.get(imginfo['cate'])] = 1.
+        # v = torch.FloatTensor([sx, sy, log(bw/aw), log(bh/ah), 1.] + clz)
         v = torch.FloatTensor([sx, sy, bw, bh, 1.] + clz)
         z[wi,hi,left:left+ceillen] = v
     return z
 
 
-xmlpath = './train_img/xmlpath/'
-files = [os.path.join(xmlpath, path) for path in os.listdir(xmlpath)]
+xmlpath = './train_img'
+files = [os.path.join(xmlpath, path) for path in os.listdir(xmlpath) if path.endswith('.xml')]
 print('load_xml_num:',len(files))
 anchors = [[121, 174]] # 这里的内容没有被实际用到，仅留后续扩展考虑。
 imginfos = [readxml(file) for file in files]
@@ -166,79 +170,71 @@ class yoloLoss(nn.Module):
         self.B = len(anchors)
         self.clazlen = len(class_types)
         self.ceillen = (5+self.clazlen)
+        # self.anchor0 = torch.FloatTensor(anchors[0]).to(DEVICE)
 
     def get_iou(self,box_pred,box_target):
         rate = 416/self.S
 
-        pre_xy = box_pred[:,:2] * rate
-        pre_wh_half = box_pred[:,2:4]/2
+        pre_xy = box_pred[...,:2] * rate
+        pre_wh_half = box_pred[...,2:4]/2
         pre_mins = pre_xy - pre_wh_half
         pre_maxs = pre_xy + pre_wh_half
-        true_xy = box_target[:,:2] * rate
-        true_wh_half = box_target[:,2:4]/2
+        true_xy = box_target[...,:2] * rate
+        true_wh_half = box_target[...,2:4]/2
         true_mins = true_xy - true_wh_half
         true_maxs = true_xy + true_wh_half
 
         inter_mins = torch.max(true_mins, pre_mins)
         inter_maxs = torch.min(true_maxs, pre_maxs)
         inter_wh   = torch.max(inter_maxs - inter_mins, torch.FloatTensor([0.]).to(DEVICE))
-        inter_area = inter_wh[:,0] * inter_wh[:,1]
-        ture_area = box_pred[:,2] * box_pred[:,3]
-        pred_area = box_target[:,2] * box_target[:,3]
+        inter_area = inter_wh[...,0] * inter_wh[...,1]
+        ture_area = box_pred[...,2] * box_pred[...,3]
+        pred_area = box_target[...,2] * box_target[...,3]
         IOUS = inter_area/(ture_area+pred_area-inter_area)
-        # print('true_xy',true_xy)
-        # print('pre_xy',pre_xy)
-        # print('inter_wh',inter_wh)
-        # print(inter_area, inter_maxs - inter_mins)
-        # print('inter_maxs',inter_maxs)
-        # print('inter_mins',inter_mins)
-        # print('true_maxs',true_maxs, pre_maxs)
-        # print('true_mins',true_mins, pre_mins)
-        # print('ture_area',ture_area)
-        # print('pred_area',pred_area)
-        # print('inter_area', inter_area)
         return IOUS
 
     def forward(self,pred_tensor,target_tensor):
         N = pred_tensor.size()[0]
-        coo_mask = target_tensor[:,:,:,4] > 0
-        noo_mask = target_tensor[:,:,:,4] == 0
-        coo_mask = coo_mask.unsqueeze(-1).expand_as(target_tensor)
-        noo_mask = noo_mask.unsqueeze(-1).expand_as(target_tensor)
-        coo_pred = pred_tensor[coo_mask].view(-1,self.ceillen).to(DEVICE)
-        box_pred = coo_pred[:,0:5].contiguous().view(-1,5).to(DEVICE)
-        class_pred = coo_pred[:,5:5+self.clazlen]
-        coo_target = target_tensor[coo_mask].view(-1,self.ceillen).to(DEVICE)
-        box_target = coo_target[:,0:5].contiguous().view(-1,5).to(DEVICE)
-        class_target = coo_target[:,5:5+self.clazlen]
+        coo_mask     = target_tensor[:,:,:,4] > 0
+        noo_mask     = target_tensor[:,:,:,4] == 0
+        coo_mask     = coo_mask.unsqueeze(-1).expand_as(target_tensor)
+        noo_mask     = noo_mask.unsqueeze(-1).expand_as(target_tensor)
+        coo_pred     = pred_tensor[coo_mask].view(N,-1,self.ceillen).to(DEVICE)
+        coo_target   = target_tensor[coo_mask].view(N,-1,self.ceillen).to(DEVICE)
+        box_pred     = coo_pred[...,0:5].contiguous().view(N,-1,5).to(DEVICE)
+        box_target   = coo_target[...,0:5].contiguous().view(N,-1,5).to(DEVICE)
+        class_pred   = coo_pred[...,5:5+self.clazlen]
+        class_target = coo_target[...,5:5+self.clazlen]
 
         # 使用torch的函数时候，要注意会在执行点进行训练处理，所以不能将该处的处理滞后
         # 这里可以很重要，因为容易变踩坑，滞后的话训练会出现异常
-        box_pred[:,4] = torch.sigmoid(box_pred[:,4])
-        box_pred[:,:2] = torch.sigmoid(box_pred[:,:2])
+        box_pred[...,4]  = torch.sigmoid(box_pred[...,4])
+        box_pred[...,:2] = torch.sigmoid(box_pred[...,:2])
+        # box_pred[...,2:4] = torch.exp(box_pred[...,2:4])*self.anchor0
         # 非目标区将降低误差处理
-        noo_pred = pred_tensor[noo_mask].view(-1,self.ceillen,N).to(DEVICE)
-        noo_target = target_tensor[noo_mask].view(-1,self.ceillen,N).to(DEVICE)
+        noo_pred   = pred_tensor[noo_mask].view(N,-1,self.ceillen).to(DEVICE)
+        noo_target = target_tensor[noo_mask].view(N,-1,self.ceillen).to(DEVICE)
         # 置信度
         IOUS = self.get_iou(box_pred,box_target)
-        print(noo_pred.shape, noo_target.shape, IOUS.shape)
-        contain_loss = F.mse_loss(box_pred[:,4]*IOUS,box_target[:,4],reduction='sum')
-        not_contain_loss = F.mse_loss(noo_pred[:,4]*IOUS,noo_target[:,4],reduction='sum')*.25
+        box_contain_loss = F.mse_loss(box_pred[...,4]*IOUS,box_target[...,4],reduction='sum')
+        noo_contain_loss = F.mse_loss(noo_pred[...,4]*IOUS,noo_target[...,4],reduction='sum')*.5
         # 坐标点的误差
-        locxy_loss = F.mse_loss(box_pred[:,:2],box_target[:,:2],reduction='sum')
-        locwh_loss = F.mse_loss(box_pred[:,2:4],box_target[:,2:4],reduction='sum')
-        loc_loss = locxy_loss + locwh_loss
+        locxy_loss = F.mse_loss(box_pred[...,:2],box_target[...,:2],reduction='sum')
+        locwh_loss = F.mse_loss(box_pred[...,2:4],box_target[...,2:4],reduction='sum')
+        loc_loss   = locxy_loss + locwh_loss
         # 分类误差
         class_loss = F.mse_loss(class_pred,class_target,reduction='sum')
+        # 全部误差
+        all_loss = (box_contain_loss + noo_contain_loss + loc_loss + class_loss)/N
         # print('[ IOUS ]              :', IOUS)
-        # print('[ contain_loss ]      :', contain_loss)
-        # print('[ not_contain_loss ]  :', not_contain_loss)
+        # print('[ box_contain_loss ]  :', box_contain_loss)
+        # print('[ noo_contain_loss ]  :', noo_contain_loss)
         # print('[ loc_loss ]          :', loc_loss)
         # print('[  |locxy_loss ]      :', locxy_loss)
         # print('[  |locwh_loss ]      :', locwh_loss)
         # print('[ class_loss ]        :', class_loss)
-        print('[ all_loss ]          :', contain_loss + not_contain_loss + loc_loss + class_loss)
-        return contain_loss + not_contain_loss + loc_loss + class_loss
+        print('[ all_loss ]          :', all_loss)
+        return all_loss
 
 
 
@@ -256,6 +252,45 @@ class yoloLoss(nn.Module):
 
 
 
+def parse_y_pred(ypred):
+    if USE_CUDA:
+        a = ypred[:,:,:,4].cpu().detach().numpy()
+    else:
+        a = ypred[:,:,:,4].detach().numpy()
+    nn = None
+    mm = float('inf')
+    for ii,i in enumerate(a[0]):
+        for jj,j in enumerate(i):
+            if j > mm or nn == None:
+                nn = (ii,jj)
+                mm = j
+    gap = 416/ypred.shape[1]
+    x, y = nn
+    contain = torch.sigmoid(ypred[0,x,y,4])
+    pred_xy = (torch.sigmoid(ypred[0,x,y,0:2]))
+    pred_wh = (ypred[0,x,y,2:4])
+    pred_clz = ypred[0,x,y,5:5+len(class_types)]
+    if USE_CUDA:
+        pred_xy = pred_xy.cpu().detach().numpy()
+        pred_wh = pred_wh.cpu().detach().numpy()
+        pred_clz = pred_clz.cpu().detach().numpy()
+    else:
+        pred_xy = pred_xy.detach().numpy()
+        pred_wh = pred_wh.detach().numpy()
+        pred_clz = pred_clz.detach().numpy()
+    cx, cy = map(float, pred_xy)
+    rx, ry = int((cx + x)*gap), int((cy + y)*gap)
+    rw, rh = map(float, pred_wh)
+    clz_   = list(map(float, pred_clz))
+    l = int(rx - rw/2)
+    r = int(rx + rw/2)
+    t = int(ry - rh/2)
+    b = int(ry + rh/2)
+    for key in class_types:
+        if clz_.index(max(clz_)) == class_types[key]:
+            clz = key
+            break
+    return (t,l,b,r), clz
 
 
 
@@ -264,85 +299,57 @@ class yoloLoss(nn.Module):
 
 
 
-EPOCH = 1
-BATCH_SIZE = 2
-LR = 0.001
-train_loader = Data.DataLoader(
-    dataset=train_data,
-    batch_size=BATCH_SIZE,
-    # shuffle=True,
-)
-net = Mini(anchors, class_types)
-optimizer = torch.optim.Adam(net.parameters(), lr=LR)
-net.to(DEVICE)
-yloss = yoloLoss(13, anchors=anchors, class_types=class_types, )
-with torch.no_grad():
-    x_test = train_data[0][0]
-    y_test = train_data[0][1]
-# 测试代码，目前只抽取一张图片无限训练，这样可以很快测试这张图片是否存在收敛
-# 正式使用请改成训练全部图片的代码
-for epoch in range(EPOCH):
-    for step, (x_true_, y_true_) in enumerate(train_loader):
-    #     break
-    # for step in range(100):
-        x_true = Variable(x_true_).to(DEVICE)
-        y_true = Variable(y_true_).to(DEVICE)
-        output = net(x_true)
-        loss = yloss(output, y_true)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        x_pred_test = net(x_test.unsqueeze(0).to(DEVICE))
 
-        def log_losinfo(x_pred_test):
-            if USE_CUDA:
-                a = x_pred_test[:,:,:,4].cpu().detach().numpy()
-            else:
-                a = x_pred_test[:,:,:,4].detach().numpy()
-            nn = None
-            mm = float('inf')
-            for ii,i in enumerate(a[0]):
-                for jj,j in enumerate(i):
-                    if j > mm or nn == None:
-                        nn = (ii,jj)
-                        mm = j
-            print(x_pred_test.shape[1])
-            gap = 416/x_pred_test.shape[1]
-            x, y = nn
-            contain = torch.sigmoid(x_pred_test[0,x,y,4])
-            pred_xy = (torch.sigmoid(x_pred_test[0,x,y,0:2]))
-            pred_wh = (x_pred_test[0,x,y,2:4])
-            pred_clz = x_pred_test[0,x,y,5:5+len(class_types)]
-            if USE_CUDA:
-                pred_xy = pred_xy.cpu().detach().numpy()
-                pred_wh = pred_wh.cpu().detach().numpy()
-                pred_clz = pred_clz.cpu().detach().numpy()
-            else:
-                pred_xy = pred_xy.detach().numpy()
-                pred_wh = pred_wh.detach().numpy()
-                pred_clz = pred_clz.detach().numpy()
-            cx, cy = map(float, pred_xy)
-            rx, ry = int((cx + x)*gap), int((cy + y)*gap)
-            rw, rh = map(int, pred_wh)
-            clz_   = list(map(float, pred_clz))
-            l = int(rx - rw/2)
-            r = int(rx + rw/2)
-            t = int(ry - rh/2)
-            b = int(ry + rh/2)
-            
-            for key in class_types:
-                if clz_.index(max(clz_)) == class_types[key]:
-                    clz = key
-                    break
-            print('rect(t,l,b,r), clz', (t,l,b,r), clz)
-            return (t,l,b,r), clz
-        print(step)
-        if step%10 == 0:
-            rect, clz = log_losinfo(x_pred_test)
 
-print('end.')
-torch.save(net, 'net.pkl')
-print('save.')
+
+
+
+
+
+
+
+def train():
+    EPOCH = 100
+    BATCH_SIZE = 10
+    LR = 0.001
+    train_loader = Data.DataLoader(
+        dataset=train_data,
+        batch_size=BATCH_SIZE,
+        shuffle=True,
+    )
+    try:
+        net = torch.load('net.pkl').to(DEVICE)
+    except:
+        net = Mini(anchors, class_types)
+    optimizer = torch.optim.Adam(net.parameters(), lr=LR)
+    net.to(DEVICE)
+    yloss = yoloLoss(13, anchors=anchors, class_types=class_types, )
+    with torch.no_grad():
+        x_test = train_data[0][0]
+        y_test = train_data[0][1]
+    # 测试代码，目前只抽取一张图片无限训练，这样可以很快测试这张图片是否存在收敛
+    # 正式使用请改成训练全部图片的代码
+    for epoch in range(EPOCH):
+        print('epoch', epoch)
+        for step, (x_true_, y_true_) in enumerate(train_loader):
+        #     break
+        # for step in range(100):
+            print('[{:<3}]'.format(step), end='')
+
+            x_true = Variable(x_true_).to(DEVICE)
+            y_true = Variable(y_true_).to(DEVICE)
+            output = net(x_true)
+            loss = yloss(output, y_true)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if step%10 == 0:
+                y_pred = net(x_test.unsqueeze(0).to(DEVICE))
+                rect, clz = parse_y_pred(y_pred)
+
+    print('end.')
+    torch.save(net, 'net.pkl')
+    print('save.')
 
 
 
@@ -360,7 +367,18 @@ def drawrect_and_show(imgfile, rect, text):# 只能写英文
     cv2.imshow('test', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-print(imginfos[0]['numpy'].shape)
-print(imginfos[0]['rect'])
-print(imginfos[0]['cate'])
-drawrect_and_show(imginfos[0]['file'], rect, clz)
+def test():
+    net = torch.load('net.pkl').to(DEVICE)
+    with torch.no_grad():
+        for i in range(30):
+            x_test = train_data[i][0]
+            y_pred = net(x_test.unsqueeze(0).to(DEVICE))
+            rect, clz = parse_y_pred(y_pred)
+            print(imginfos[i]['numpy'].shape)
+            print(imginfos[i]['rect'])
+            print(imginfos[i]['cate'])
+            drawrect_and_show(imginfos[i]['file'], rect, clz)
+
+
+# train()
+test()
