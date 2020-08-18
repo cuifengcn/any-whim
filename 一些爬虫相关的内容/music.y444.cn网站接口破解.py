200b6dd419b989906aa9184acff9de4f23ded7b2
opaquism <opaquism@hotmail.com>
2020-08-18 12:33:02 +0800

    music.y444.cn

--- 一些爬虫相关的内容/music.y444.cn网站接口破解.py
+++ 一些爬虫相关的内容/music.y444.cn网站接口破解.py
@@ -85,10 +85,10 @@ pprint.pprint(jsondata)
 import re, json
 from urllib import request, parse
 from urllib.parse import urlencode
-def mk_url_headers_body(songkey):
+def mk_url_headers_body(songid):
     url = 'http://music.y444.cn/api/v1/jx/v4/'
     headers = { "User-Agent": "Chrome/84.0.4147.125 Safari/537.36" }
-    body = { "params": enc_post_params(json.dumps({"id":songkey,"q":"128","src":"wyy","play":True})), # q代表质量固定即可，这里的src就是网易云固定即可，play固定即可，所以这里你只需要音乐id的参数即可
+    body = { "params": enc_post_params(json.dumps({"id":songid,"q":"128","src":"wyy","play":True})), # q代表质量固定即可，这里的src就是网易云固定即可，play固定即可，所以这里你只需要音乐id的参数即可
              "sec_key": "SMGnQH2eEpzq9CNl8QNA86De2yjyRxWltD/KCeg+i9SjR3o+QjgUPPeKIy+Batt3cNQr3wBrqTAvn7SccdK7QyPRlOW6aFeg9dxGpEYQEBwKjqYqz7BUPvijJVbYkE+/HRaJpLutephcENsy1zKeRk95ciyO6MLvtRJG7mi3an0=" }
     body = urlencode(body).encode('utf-8')
     return url,headers,body
@@ -97,8 +97,8 @@ def mypost(url, headers, body):
     for k, v in list(headers.items()): r.add_header(k, v)
     opener = request.build_opener(request.ProxyHandler(None))
     return opener.open(r, data=body)
-songkey = 414817
-url, headers, body = mk_url_headers_body(songkey)
+songid = 414817
+url, headers, body = mk_url_headers_body(songid)
 content = mypost(url, headers, body).read()
 jsondata = json.loads(content)
 jsondata = dec_retrun_info(jsondata['data'])
