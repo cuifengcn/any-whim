b5130957890ed35518bbf38c824574453d59709f
cilame <opaquism@hotmail.com>
2020-11-05 21:38:24 +0800

    add

--- 关于c语言/she11code_execute.c
+++ 关于c语言/she11code_execute.c
@@ -1,9 +1,7 @@
 // 注意，编译工具编译的环境，32位的 shellcode 只能使用 32位编译出来的工具
-
 #include <stdio.h>
 #include <windows.h>
 #pragma comment(lib, "user32")
-
 int main(int argc, char const *argv[]) {
     HANDLE hFile = CreateFileA(argv[1], GENERIC_READ, 0, NULL, OPEN_ALWAYS, 0, NULL);
     if (hFile == INVALID_HANDLE_VALUE){
@@ -21,7 +19,6 @@ int main(int argc, char const *argv[]) {
     DWORD dwRead;
     ReadFile(hFile, lpAddress, dwSize, &dwRead, 0);
     ((void(*)(void))lpAddress)();
-
     MessageBoxA(NULL,"this box is show means that shellcode dnot kill this execute.", "test", 0);
     return 0;
 }
