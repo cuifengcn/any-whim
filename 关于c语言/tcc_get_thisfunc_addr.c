a806639425b01ab98e51d93d54eca188228d5255
cilame <opaquism@hotmail.com>
2020-11-12 02:07:04 +0800

    add

--- 关于c语言/tcc_get_thisfunc_addr.c
+++ 关于c语言/tcc_get_thisfunc_addr.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
 #include <windows.h>
 
-// get_nextfunc_realaddr 用来获取这个函数后面一个函数的绝对地址
+// get_thisfunc_realaddr 用来获取这个函数自己的地址
 // 对于一些 shellcode 来说会有比较大的用处。
 // gcc/tcc 32/64 通用的函数
 
-void AdjustPE();
-DWORD get_nextfunc_realaddr(){
+DWORD get_thisfunc_realaddr(){
     // 这个函数非常娇贵，一丝一毫也不能改。
     #ifdef _WIN64
     int _addr;
     asm("mov $., %0;":"=a"(_addr));
         #ifdef __MINGW32__
-        return (DWORD)_addr+0x14; 
+        return (DWORD)_addr-0x8; 
         #else
-        return (DWORD)_addr-0xb + 0x28;
+        return (DWORD)_addr-0xb;
         #endif
     #else
     int _addr;
     asm("mov $., %0;":"=a"(_addr));
         #ifdef __MINGW32__
-        return (DWORD)_addr-0x6 + 0x16;
+        return (DWORD)_addr-0x6;
         #else
-        return (DWORD)_addr-0xa + 0x1d;
+        return (DWORD)_addr-0xa;
         #endif
     #endif
 }
-void AdjustPE() {
-}
 
 int main(){
-    printf("%x\n", AdjustPE);
-    printf("%x\n", get_nextfunc_realaddr());
+    printf("%x\n", get_thisfunc_realaddr);
+    printf("%x\n", get_thisfunc_realaddr());
 }
