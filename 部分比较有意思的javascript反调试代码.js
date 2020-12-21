ec2f3a42ba1d8a67bf4b29e392204b319805c98d
opaquism <opaquism@hotmail.com>
2020-12-21 15:21:40 +0800

    add

--- 部分比较有意思的javascript反调试代码.js
+++ 部分比较有意思的javascript反调试代码.js
@@ -9,12 +9,35 @@ setTimeout(function adbg() {
   setTimeout(adbg, 200);
 }, 200)
 
-// 钩子函数的实现
-const handler = {
-  apply: function (target, thisArg, args){
-    console.log("Intercepted a call tocreateElement with args: " + args);
-    return target.apply(thisArg, args)
+// 挂钩内置函数 window.eval
+// 如果想挂钩别的，可以将 window.eval 直接替换成别的即可，例如 document.createElement
+(function(){
+  eval_string = window.eval.toString()
+  const handler = {
+    apply: function (target, thisArg, args){
+      console.log("Intercepted a call eval with args: " + args);
+      return target.apply(thisArg, args)
+    }
   }
-}
-document.createElement = new Proxy(document.createElement, handler);
-document.createElement.toString = Function.prototype.toString
+  const handler_tostring = {
+    apply: function (target, thisArg, args){
+      return eval_string;
+    }
+  }
+  window.eval = new Proxy(window.eval, handler);
+  window.eval.toString = new Proxy(window.eval.toString, handler_tostring);
+})();
+
+// 挂钩 cookie 生成的时机。
+(function(){
+  'use strict';
+  Object.defineProperty(
+    document, 'cookie', {
+      set: function(cookie){
+        // if (cookie.indexOf('RM4hZBv0dDon443M') != -1){ debugger; }
+        debugger;
+        return cookie;
+      }
+    }
+  )
+})();
