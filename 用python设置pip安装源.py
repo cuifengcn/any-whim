eca920bf2fe11ad2955c6daa954925ba290becf2
opaquism <opaquism@hotmail.com>
2021-01-10 20:01:40 +0800

    add

--- 用python设置pip安装源.py
+++ 用python设置pip安装源.py
@@ -7,6 +7,18 @@ pipfile = os.path.join(pippath, 'pip.ini')
 if not os.path.isdir(pippath):
     os.mkdir(pippath)
 
+diclist = [
+    ['http://pypi.douban.com/simple/', '豆瓣'],
+    ['http://mirrors.aliyun.com/pypi/simple/', '阿里云'],
+    ['http://pypi.tuna.tsinghua.edu.cn/simple/', '清华大学'],
+    ['http://pypi.mirrors.ustc.edu.cn/simple/', '中国科学技术大学'],
+]
+dic_url2name = {}
+dic_name2url = {}
+for k,v in diclist:
+    dic_url2name[k] = v
+    dic_name2url[v] = k
+
 def read_setting():
     if not os.path.isfile(pipfile):
         return None
@@ -14,35 +26,23 @@ def read_setting():
         with open(pipfile) as f:
             setstr = f.read()
         mirrors = re.findall('\nmirrors = ([^\n]+)', setstr)[0]
-        dic = {
-            'http://pypi.douban.com/simple/': '豆瓣',
-            'http://mirrors.aliyun.com/pypi/simple/': '阿里云',
-            'http://pypi.tuna.tsinghua.edu.cn/simple/': '清华大学',
-            'http://pypi.mirrors.ustc.edu.cn/simple/': '中国科学技术大学',
-        }
-        return dic.get(mirrors)
+        return dic_url2name.get(mirrors)
 
 def write_setting(name=None):
     setting = '''[global]\nindex-url = {}\n[install]\nuse-mirrors = true\nmirrors = {}\ntrusted-host = {}'''.strip()
-    dic = {
-        '豆瓣': 'http://pypi.douban.com/simple/',
-        '阿里云': 'http://mirrors.aliyun.com/pypi/simple/',
-        '清华大学': 'http://pypi.tuna.tsinghua.edu.cn/simple/',
-        '中国科学技术大学': 'http://pypi.mirrors.ustc.edu.cn/simple/',
-    }
     if name is None:
         if os.path.isfile(pipfile):
             os.remove(pipfile)
         return
-    if name not in dic:
-        raise Exception("{} must in {}".format(name, list(dic)))
-    mirrors = dic.get(name)
+    if name not in dic_name2url:
+        raise Exception("{} must in {}".format(name, list(dic_name2url)))
+    mirrors = dic_name2url.get(name)
     index_url = mirrors.strip(' /')
     trusted_host = urllib.parse.urlsplit(index_url).netloc
     with open(pipfile, 'w') as f:
         f.write(setting.format(index_url, mirrors, trusted_host))
 
 if __name__ == '__main__':
-    write_setting()
+    write_setting('豆瓣')
     v = read_setting()
     print(v)
