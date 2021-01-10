c47b1c74e5a7a257bb747ab31ebf61dfa1721748
opaquism <opaquism@hotmail.com>
2021-01-10 19:09:15 +0800

    add

--- 使用easyocr识别文字.py
+++ 使用easyocr识别文字.py
@@ -8,10 +8,16 @@
 # 1 先使用pip下载项目
 # 	pip install easyocr
 # 2 找到pytorch网页，按照官网给的方式下载，（注意选择是否需要gpu的版本）
-# 3 如果出现了错误，下载对应的软件安装即可
+# 3 如果在 import easyocr 时出现了错误：
 #   Error loading "D:\Coding\Anaconda\lib\site-packages\torch\lib\asmjit.dll"
 #   下载这个软件 https://aka.ms/vs/16/release/vc_redist.x64.exe 然后安装即可
 #   如果该地址找不到软件，可以从上面的百度网盘里面找找看。
+# 4 下载模型地址，可以在上面的百度网盘里面下载对应的模型
+#   其中 craft_mlt_25k.pth 是必须的，其他语言按需下载即可。不过建议尽量全部都下载。
+#   个人主要使用 latin 和 chinese_sim 这两个，下载后将文件都放到
+#   用户路径下的 /.EasyOCR/model 文件路径下即可，
+# 	如果还不知道这个路径在哪，请直接执行下面一行脚本输出的地址就是目标地址
+#   import os;print(os.path.join(os.path.expanduser('~'),'.EasyOCR','model'))
 
 import easyocr
 reader = easyocr.Reader(['ch_sim','en'], gpu=False)
