3c6c4e47d0a54b1c8170fa4da3d3b9d86879f92f
opaquism <opaquism@hotmail.com>
2021-02-12 18:05:14 +0800

    add

--- js打包es6转es5.js
+++ js打包es6转es5.js
@@ -4,7 +4,7 @@
 // cnpm install webpack-cli webpack -S
 // cnpm install babel-cli babel-preset-env -S
 // npx webpack --entry="./index.js"
-// npx babel dist/main.js -d dist --presets=babel-preset-env
+// npx babel dist/main.js -d es5 --presets=babel-preset-env
 // 在 es5/dist 文件夹下找 main.js 就是目标文件了。
 // 可以新建一个文件夹命名为 index.js 输入下面两行内容。用上面的方式打包。
 // const CryptoJS = require('crypto-js')
